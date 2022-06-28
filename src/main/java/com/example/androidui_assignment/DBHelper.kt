11dc2e4e5416208f6cfa4ffa24b8177cfc66e1f6
cjsworkem <cjsworkem@naver.com>
2022-06-28 14:58:43 +0900

    초기 내용

---
+++ src/main/java/com/example/androidui_assignment/DBHelper.kt
@@ -0,0 +1,15 @@
+package com.example.androidui_assignment
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DBHelper(context: Context): SQLiteOpenHelper(context,"testdb",null,1) {
+    override fun onCreate(db: SQLiteDatabase?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        TODO("Not yet implemented")
+    }
+}   
---
+++ src/main/java/com/example/androidui_assignment/MyApdater.kt
@@ -0,0 +1,2 @@
+package com.example.androidui_assignment
+
